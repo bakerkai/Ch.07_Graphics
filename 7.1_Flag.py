bbbd77d15214443cc93c86f8860e2dc6fa3a5451
bakerkai <bakerkai@urbandaleschools.com>
2021-11-08 12:34:47 -0600

    13

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -10,12 +10,22 @@ We will have a competition to see who can make this flag in the least lines of c
 The record is 16! You will have to use some loops to achieve this.
 '''
 import arcade
-
-SW = 600
-SH = 400
-arcade.open_window(SW, SH, "Art that I tried to make", False, False)
+SW = 494
+redline = 10 + 40
+arcade.open_window(494, 260, "Art that I tried to make", True)
 arcade.set_background_color(arcade.color.WHITE)
 arcade.start_render()
-arcade.draw_line(100, 200, 120, 260, lk)
+arcade.draw_rectangle_filled(99, 190, 198, 140, arcade.color.BLUE)
+arcade.draw_rectangle_filled(247, 10, SW, 10, arcade.color.RED)
+arcade.draw_rectangle_filled(247, 50, SW, 10, arcade.color.RED)
+arcade.draw_rectangle_filled(247, 90, SW, 10, arcade.color.RED)
+arcade.draw_rectangle_filled(398, 130, 400, 10, arcade.color.RED)
+arcade.draw_rectangle_filled(398, 170, 400, 10, arcade.color.RED)
+arcade.draw_rectangle_filled(398, 210, 400, 10, arcade.color.RED)
+arcade.draw_rectangle_filled(398, 250, 400, 10, arcade.color.RED)
+
+
+
+
 arcade.finish_render()
 arcade.run()
