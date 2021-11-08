e49fb8d3a5cf4c08017393be2861aeb190ad1605
bakerkai <bakerkai@urbandaleschools.com>
2021-11-08 10:07:25 -0600

    h

--- 7.1_Flag.py
+++ 7.1_Flag.py
@@ -9,3 +9,13 @@ I used a draw_text command and used 20 pt. asterisks for the stars.
 We will have a competition to see who can make this flag in the least lines of code.
 The record is 16! You will have to use some loops to achieve this.
 '''
+import arcade
+
+SW = 600
+SH = 400
+arcade.open_window(SW, SH, "Art that I tried to make", False, False)
+arcade.set_background_color(arcade.color.WHITE)
+arcade.start_render()
+arcade.draw_line(100, 200, 120, 260, lk)
+arcade.finish_render()
+arcade.run()
